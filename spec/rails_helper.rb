d4c009468888c19e5e3eb0ce7a4895926b4a137b
DevCam <josean.camarena@gmail.com>
2022-08-14 18:22:01 -0500

    add testing to checkout logic

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,9 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # Rspec, my dude, why is this not default
+  config.include Rails.application.routes.url_helpers
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
--- spec/requests/shopping_carts_spec.rb
+++ spec/requests/shopping_carts_spec.rb
@@ -34,4 +34,56 @@ RSpec.describe "/shopping_carts", type: :request do
     end
   end
 
+  describe "GET /complete_purchase" do
+    context "on correct state" do
+
+      it "removes offers" do
+        shopping_cart = create(:shopping_cart, {terms_accepted: true})
+        brick = create(:brick, {on_sale: true, price: 123})
+        shopping_cart.bricks << brick
+
+        expect {
+          get complete_purchase_shopping_cart_url(shopping_cart)
+        }.to change(Offer, :count).by -1
+      end
+
+      it "adds brick to user" do
+        shopping_cart = create(:shopping_cart, {terms_accepted: true})
+        brick = create(:brick, {on_sale: true, price: 123})
+        shopping_cart.bricks << brick
+
+        get complete_purchase_shopping_cart_url(shopping_cart)
+
+        expect(shopping_cart.user.bricks).to contain_exactly(brick)
+      end
+
+      it "handles brick collisions" do
+        cart_a = create(:shopping_cart, {terms_accepted: true})
+        cart_b = create(:shopping_cart, {terms_accepted: true})
+
+        brick = create(:brick, {on_sale: true, price: 123})
+        cart_a.bricks << brick
+        cart_b.bricks << brick
+
+        # both users added the brick to their cart
+        expect {
+          get complete_purchase_shopping_cart_url(cart_a)
+          get complete_purchase_shopping_cart_url(cart_b)
+        }.to change(Offer, :count).by -1 #only one offer is resolved!
+      end
+
+
+    end
+
+    context "on incorrect state" do
+      it "does not checkout if terms are not accepted" do
+        shopping_cart = create(:shopping_cart)
+        expect {
+          get complete_purchase_shopping_cart_url(shopping_cart)
+        }.to change(Offer, :count).by(0)
+      end
+    end
+
+  end
+
 end
