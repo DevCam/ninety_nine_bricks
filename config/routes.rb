df2c9f8b096ff4c80b37a07310842ee28b5c0719
DevCam <josean.camarena@gmail.com>
2022-08-14 13:19:28 -0500

    failed users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :offers
   resources :realties, only: [:index, :show]
+
   resources :shopping_carts do
     get 'checkout', :on => :member
   end
+
   resources :users
 
   # Defines the root path route ("/")
